7595dfd1e9f69b272d3d7ae5b355a32c1bc392dd
Andrés K. Aquin <andruuus123@gmail.com>
2021-09-20 22:49:24 -0400

    Por fin, .env arreglado

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+//import Apollo Client libraries
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 //import global styles
 import GlobalStyle from './components/GlobalStyle';
 import Pages from './pages';
 
+// configure our API URI & cache
+const uri = 'http://localhost:4000/api';
+const cache = new InMemoryCache();
+
+// configure Apollo Client
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true
+});
+
 const App = () => {
   return (
-    <div>
+    <ApolloProvider client={client}>
       <GlobalStyle/>
       <Pages />
-    </div>
+    </ApolloProvider>
   );
 };
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,42 @@
 import React from "react";
 
-import Button from "../components/Button";
+import { useQuery, gql } from "@apollo/client";
+
+// our Graphql query, stored as a variable
+const GET_NOTES = gql`
+  query noteFeed($cursor: String){
+    noteFeed(cursor: $cursor) {
+      cursor
+      hasNextPage
+      notes {
+        id
+        createdAt
+        content
+        favoriteCount
+        author {
+          username
+          id
+          avatar
+        }
+      }
+    }
+  }
+`;
 
 const Home = () => {
+  // query hook
+  const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
+
+  // if the data is loading, display a loading message
+  if (loading) return <p>Loading...</p>;
+  // if there is an error fetching the data, display an error message
+  if (error) return <p>Error!</p>
+  // if the data is successful, display the data in our UI
   return (
     <div>
-      <p>This is the home page</p>
-      <Button>Click Me!</Button>
+      {data.noteFeed.notes.map(note => (
+        <div key={note.id}>{note.author.username}</div>
+      ))}
     </div>
   );
 };
